0d9a3363c88cf8b78288254be2f314a091c7c568
reliruleru00 <93340772+reliruleru00@users.noreply.github.com>
2024-08-06 02:37:10 +0900

    オーブボスLv4

--- js/damege.js
+++ js/damege.js
@@ -1007,7 +1007,7 @@ function updateEnemyResist() {
     let resist_down = getSumEffectSize("resist_down");
     let element_resist = Number(enemy_info["element_" + element]) - grade_sum["element_" + element];
     // 耐性打ち消し
-    if (resist_down > 0) {
+    if (resist_down > 0 && element_resist >= 0) {
         if (element_resist <= 100) {
             element_resist = 100 + resist_down;
         } else {
