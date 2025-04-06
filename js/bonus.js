0b39b7e095fb152fa194b3b621a1dd5bda915929
reliruleru00 <93340772+reliruleru00@users.noreply.github.com>
2025-04-06 17:21:23 +0900

    敵データのみ先行追加

--- js/bonus.js
+++ js/bonus.js
@@ -15,4 +15,7 @@
     {"score_attack_no":71,"bonus_no":2,"conditions":"element_3","effect_kind":"STAT_UP","effect_size":30},
     {"score_attack_no":71,"bonus_no":3,"conditions":"physical_3","effect_kind":"STAT_UP","effect_size":15},
     {"score_attack_no":71,"bonus_no":4,"conditions":"physical_1","effect_kind":"STAT_UP","effect_size":15},
+    {"score_attack_no":72,"bonus_no":1,"conditions":"element_1","effect_kind":"STAT_UP","effect_size":30},
+    {"score_attack_no":72,"bonus_no":3,"conditions":"physical_2","effect_kind":"STAT_UP","effect_size":15},
+    {"score_attack_no":72,"bonus_no":4,"conditions":"physical_3","effect_kind":"STAT_UP","effect_size":15},
 ];
