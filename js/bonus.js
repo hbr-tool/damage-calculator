258d3b8995a5343de6dc9489122587ca0bf468a3
reliruleru00 <93340772+reliruleru00@users.noreply.github.com>
2025-05-16 01:28:14 +0900

    20250516更新分、スコアタ以外

--- js/bonus.js
+++ js/bonus.js
@@ -22,4 +22,8 @@
     {"score_attack_no":73,"bonus_no":2,"conditions":"element_1","effect_kind":"STAT_UP","effect_size":30},
     {"score_attack_no":73,"bonus_no":3,"conditions":"physical_3","effect_kind":"STAT_UP","effect_size":15},
     {"score_attack_no":73,"bonus_no":4,"conditions":"physical_2","effect_kind":"STAT_UP","effect_size":15},
+    {"score_attack_no":74,"bonus_no":1,"conditions":"element_5","effect_kind":"STAT_UP","effect_size":30},
+    {"score_attack_no":74,"bonus_no":2,"conditions":"element_4","effect_kind":"STAT_UP","effect_size":30},
+    {"score_attack_no":74,"bonus_no":3,"conditions":"physical_3","effect_kind":"STAT_UP","effect_size":15},
+    {"score_attack_no":74,"bonus_no":4,"conditions":"physical_1","effect_kind":"STAT_UP","effect_size":15},
 ];
--- js/spod_sim.js
+++ js/spod_sim.js
@@ -1,7 +1,7 @@
 let select_troops = localStorage.getItem('select_troops');
 let select_style_list = Array(6).fill(undefined);
 // 使用不可スタイル
-const NOT_USE_STYLE = [36];
+const NOT_USE_STYLE = [36, 164];
 // 制限アビリティ
 const CONSTRAINTS_ABILITY = [
     1136, // 勝勢
