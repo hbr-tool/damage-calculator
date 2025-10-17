5187eb6a37fc4c9006d6840373aa37dd565201d5
reliruleru00 <93340772+reliruleru00@users.noreply.github.com>
2025-10-17 20:17:05 +0900

    20251017敵データ

--- src/data/scoreBonus.js
+++ src/data/scoreBonus.js
@@ -15,6 +15,9 @@ const scoreBonus = [
     {"score_attack_no":83,"half":1,"bonus_no":1,"conditions":"element_4","effect_kind":"STAT_UP","effect_size":30},
     {"score_attack_no":83,"half":1,"bonus_no":2,"conditions":"physical_1","effect_kind":"STAT_UP","effect_size":15},
     {"score_attack_no":83,"half":1,"bonus_no":3,"conditions":"physical_2","effect_kind":"STAT_UP","effect_size":15},
+    {"score_attack_no":84,"half":1,"bonus_no":1,"conditions":"element_2","effect_kind":"STAT_UP","effect_size":30},
+    {"score_attack_no":84,"half":1,"bonus_no":2,"conditions":"element_4","effect_kind":"STAT_UP","effect_size":30},
+    {"score_attack_no":84,"half":1,"bonus_no":3,"conditions":"physical_1","effect_kind":"STAT_UP","effect_size":15},
 ];
 
 export default scoreBonus;
